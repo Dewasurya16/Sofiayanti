f861d2484fa0e8ed27cd643c6b3f7dd08cfc01e2
Dewa Sinar Surya <55642480+Dewasurya16@users.noreply.github.com>
2021-06-08 09:09:30 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Ketika sofia marah apa yang harus dilakukan?",
+    answer: "Sabar dan tahan emosi",
+    options: [
+      "Banting Banting barang kagak jelas",
+      "Cuekin semua orang Termasuk aku :(",
+      "Sabar dan tahan emosi",
+      "Gak Taulah masa tanya saya!!"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Apa bedanya kamu sama garuda??",
+    answer: "Garuda di dadaku, kalau kamu di hatiku",
+    options: [
+      "Garuda di dadaku, kalau kamu di hatiku",
+      "Gak tau kak ",
+      "Kamu jangan marah terus dong wkkw",
+      "Kamu jangan ngambek terus dong sof !"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Apa bedanya kamu sama kipas? ",
+    answer: "Kipas bisa bikin angin, kalo kamu bisa bikin kangen",
+    options: [
+      "Aku to pengen kamu tuh ga marah marah terus",
+      "Ingat tahannnnn yaaaaa marahnya",
+      "Kipas bisa bikin angin, kalo kamu bisa bikin kangen",
+      "Sofia kuat kok"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Apa perbedaan kamu dengan modem?",
+    answer: "Modem terhubung ke internet kalau kamu terhubung ke hatimu",
+    options: [
+      "Kamu tau gak sih sofia aku to gak nyaman nek koe marah wwwkkw",
+      "Modem terhubung ke internet kalau kamu terhubung ke hatimu",
+      "Kadang suka kepikiran aja ga tau kenapa ",
+      "jawab dah buru wkwkkw"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Kopi apa yang paling enak? ",
+    answer: "Kopilih aku jadi pasanganmu",
+    options: [
+      "WKWKWK cringe ya",
+      "Kopilih aku jadi pasanganmu",
+      "Ditolak berkali kali tetep no nyerah kan wkwkkw",
+      "buruuuuuuuu jawab dah wkkwkwkwk"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
